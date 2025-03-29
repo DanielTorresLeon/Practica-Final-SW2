63f0a523a346934aa7b57570e8bab59b48b3ac40
chonrc <chonrc00@estudiantes.unileon.es>
2025-03-29 09:38:52 +0100

    Finished User Registration

--- AutonoMeet_backend/app/__init__.py
+++ AutonoMeet_backend/app/__init__.py
@@ -9,6 +9,8 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+
+    API_PREFIX = '/api/v0'
     
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -21,10 +23,12 @@ def create_app():
     db.init_app(app)
     
     with app.app_context():
+        from app.models.user import User
         db.create_all()
     
     api = Api(app, title="AutonoMeetApi", version="1.0", description="Documentation of AutonoMeet API with Flask-RESTx")
 
-
+    from app.routes import auth_routes
+    api.add_namespace(auth_routes.api, path=f"{API_PREFIX}/auth") 
 
     return app
