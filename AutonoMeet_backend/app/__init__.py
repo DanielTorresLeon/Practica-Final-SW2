a5ddf7ce29784cd85108c1dafeb91f96b1ce7c39
chonrc <chonrc00@estudiantes.unileon.es>
2025-03-29 09:17:31 +0100

    Conntcted app to the db

--- AutonoMeet_backend/app/__init__.py
+++ AutonoMeet_backend/app/__init__.py
@@ -1,12 +1,30 @@
 from flask import Flask
 from flask_restx import Api
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+
+db = SQLAlchemy()
+
 
 def create_app():
     app = Flask(__name__)
+    
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+        'pool_size': 10,
+        'pool_recycle': 300,
+        'pool_pre_ping': True
+    }
+    
+    db.init_app(app)
+    
+    with app.app_context():
+        db.create_all()
+    
     api = Api(app, title="AutonoMeetApi", version="1.0", description="Documentation of AutonoMeet API with Flask-RESTx")
 
-    from .routes.product_routes import ns as product_ns
-    api.add_namespace(product_ns, path="/products")
+
 
     return app
-
