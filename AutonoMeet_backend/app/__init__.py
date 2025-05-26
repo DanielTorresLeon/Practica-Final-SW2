da5d6839a0aef10039e0c4710792c55501246626
Daniel <91316017+DanielTorresLeon@users.noreply.github.com>
2025-05-27 00:35:53 +0200

    Updated cors for accepting all origins

--- AutonoMeet_backend/app/__init__.py
+++ AutonoMeet_backend/app/__init__.py
@@ -19,7 +19,7 @@ def create_app():
 
     CORS(
         app,
-        origins="http://localhost:5173",  
+        origins="*",  
         methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],  
         allow_headers=["Content-Type", "Authorization"],  
         supports_credentials=True  
