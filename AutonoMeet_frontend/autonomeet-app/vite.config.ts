ddf0b667a18b97743974fa9eb47429a33122af29
chonrc <chonrc00@estudiantes.unileon.es>
2025-05-30 14:51:40 +0200

    removed test dependencies

--- AutonoMeet_frontend/autonomeet-app/vite.config.ts
+++ AutonoMeet_frontend/autonomeet-app/vite.config.ts
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Escuchar en todas las interfaces (necesario para Render)
-    port: 5173, // Puerto predeterminado de Vite
+    host: '0.0.0.0', 
+    port: 5173, 
     allowedHosts: [
-      'practica-final-sw2-frontend.onrender.com', // Permitir el dominio de Render
-      'localhost', // Para pruebas locales
+      'practica-final-sw2-frontend.onrender.com', 
+      'localhost', 
     ],
   },
 });
